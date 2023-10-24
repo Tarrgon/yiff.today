68d2ea042f73e00ad0eff1b4439762ba10bd4bf5
DontTalkToMeThx <extras@is-just-a.dev>
2023-10-24 02:27:56 -0400

    No queue

--- src/website/public/js/e621AutoCompleter.js
+++ src/website/public/js/e621AutoCompleter.js
@@ -1,27 +1,26 @@
 let e621AutoComplete = {
-  queue: [],
+  next: null,
+  current: null,
   processQueue: async () => {
-    let item = e621AutoComplete.queue[0]
-    let res = await fetch(`https://e621.net/tags/autocomplete.json?search%5Bname_matches%5D=${item.query}&expiry=7`)
+    let current = e621AutoComplete.current = e621AutoComplete.next
+    e621AutoComplete.next = null
+    let res = await fetch(`https://e621.net/tags/autocomplete.json?search%5Bname_matches%5D=${current.query}&expiry=7`)
 
     if (res.ok) {
-      item.resolve(await res.json())
+      current.resolve(await res.json())
     } else {
-      item.reject(res)
+      current.reject(res)
     }
 
     await wait(10)
-    e621AutoComplete.queue.shift()
-
-    if (e621AutoComplete.queue.length > 0) {
-      e621AutoComplete.processQueue()
-    }
+    e621AutoComplete.current = null
+    if (e621AutoComplete.next) e621AutoComplete.processQueue()
   },
 
   autoComplete: (query) => {
     return new Promise((resolve, reject) => {
-      e621AutoComplete.queue.push({ query, resolve, reject })
-      if (e621AutoComplete.queue.length == 1) e621AutoComplete.processQueue()
+      e621AutoComplete.next = { query, resolve, reject }
+      if (e621AutoComplete.current == null) e621AutoComplete.processQueue()
     })
   }
 }
